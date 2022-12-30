70f78df27457b47ef1fec2bfda777ab6626fd531
jaredoconnell <jared.oc321@gmail.com>
2022-12-30 00:43:58 -0500

    Failed attempt at full duplex
    The different parts run sequentially

--- src/client.rs
+++ src/client.rs
@@ -1,8 +1,20 @@
 use futures::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
-    try_join,
+    try_join, join
 };
 use interprocess::local_socket::{tokio::LocalSocketStream};
+use std::thread;
+use std::time;
+use std::time::{SystemTime, UNIX_EPOCH};
+use std::sync::{Arc, Mutex};
+
+fn get_time_ms() -> u64 {
+    let start = SystemTime::now();
+    let since_the_epoch = start
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards");
+    since_the_epoch.as_secs()
+}
 
 pub async fn main() -> anyhow::Result<()> {
     // Pick a name. There isn't a helper function for this, mostly because it's largely unnecessary:
@@ -11,6 +23,9 @@ pub async fn main() -> anyhow::Result<()> {
 
     // Await this here since we can't do a whole lot without a connection.
     let conn = LocalSocketStream::connect(name).await?;
+
+    //let mutex_1 = Arc::new(Mutex::new(0));
+    //let mutex_2 = mutex_1.clone();
 
     // This consumes our connection and splits it into two halves,
     // so that we could concurrently act on both.
@@ -21,7 +36,7 @@ pub async fn main() -> anyhow::Result<()> {
     // This size should be enough and should be easy to find for the allocator.
     let mut buffer = String::with_capacity(128);
 
-    let read = reader.read_line(&mut buffer);
+    /*let read = reader.read_line(&mut buffer);
     let join_result = try_join!(read);
 
     match join_result {
@@ -31,43 +46,83 @@ pub async fn main() -> anyhow::Result<()> {
         Err(e) => {
             eprintln!("Error reading first message from server: {:?}", e);
         },
-    }
+    }*/
 
-    for i in 0..20 {
-        println!("Sending data #{} to server", i);
+    // Loop until EOF
+    // Allocate a sizeable buffer for reading.
+    // This size should be enough and should be easy to find for the allocator.
+    let mut buffer = String::with_capacity(128);
 
-        // Describe the write operation as writing our whole string.
-        let write = writer.write_all(b"Hello from client!\n");
-        // Describe the read operation as reading until a newline into our buffer.
-        //
+    let read_async_handle = async {
+        let mut total_read = 0;
+        loop {                        
+            //let guard = mutex_1.lock();
+            // Describe the read operation as reading into our big buffer.
+            let read = reader.read_line(&mut buffer);
+            total_read += 1;
 
-        // Concurrently perform both operations.
-        //try_join!(write, read)?;
+            let read_result = read.await;
+            //drop(guard);
+
+            if read_result.is_err() {
+                println!("Error reading result in client. Exiting. Total read: {}", total_read);
+                break;
+            }
+
+            if buffer.contains('\0') {
+                println!("End acknowldged received on client side. Exiting. Total read: {}", total_read);
+                break;
+            } else {
+                println!("{} Got from server: {}", get_time_ms(), buffer.as_str());
+                buffer.clear();
+            }
+        }
+        drop(reader);
+    };
+    
+
+    let write_async_handle = async {
+        for i in 0..5 {
+            //let guard = mutex_2.lock();
+            println!("Sending data #{} to server at {}", i, get_time_ms());
+
+            // Describe the write operation as writing our whole string.
+            let write = writer.write_all(b"Hello from client!\n");
+            // Describe the read operation as reading until a newline into our buffer.
+            //
+
+            // Concurrently perform both operations.
+            //try_join!(write, read)?;
+            let write_join_result = try_join!(write);
+            //drop(guard);
+            match write_join_result {
+                Ok(_) => {
+                    println!("Successfully wrote to server");
+                },
+                Err(e) => {
+                    eprintln!("Error writing #{} message to server: {}", i, e);
+                },
+            }
+            thread::sleep(time::Duration::from_millis(1000));
+        }
+
+        let write = writer.write_all(b"\0\n");
         let write_join_result = try_join!(write);
         match write_join_result {
             Ok(_) => {
-                println!("Successfully wrote to server");
+                println!("Successfully wrote null (end) to server");
             },
             Err(e) => {
-                eprintln!("Error writing #{} message to server: {}", i, e);
+                eprintln!("Error writing null end message to server: {}", e);
             },
         }
-    }
+    };
 
-    let write = writer.write_all(b"\0");
-    let write_join_result = try_join!(write);
-    match write_join_result {
-        Ok(_) => {
-            println!("Successfully wrote null (end) to server");
-        },
-        Err(e) => {
-            eprintln!("Error writing null end message to server: {}", e);
-        },
-    }
+    join!(read_async_handle, write_async_handle);
 
 
     // Close the connection a bit earlier than you'd think we would. Nice practice!
-    drop((reader, writer));
+    drop(writer);
 
     // Describe the write operation as writing our whole string.
     println!("Server answered: {}", buffer.trim());
--- src/server.rs
+++ src/server.rs
@@ -1,48 +1,107 @@
 use futures::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
-    try_join,
+    try_join, join,
 };
-use interprocess::local_socket::{
-    tokio::{LocalSocketListener, LocalSocketStream},
+use interprocess::local_socket::tokio::{
+    LocalSocketListener, LocalSocketStream,
 };
 use std::io;
 use tokio::sync::oneshot::Sender;
+use std::thread;
+use std::time;
+use std::sync::atomic::AtomicBool;
+use std::sync::atomic::Ordering;
+use std::sync::{Arc, Mutex};
+use std::time::{SystemTime, UNIX_EPOCH};
+
+
+fn get_time_ms() -> u64 {
+    let start = SystemTime::now();
+    let since_the_epoch = start
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards");
+    since_the_epoch.as_secs()
+}
 
 async fn handle_conn(conn: LocalSocketStream) -> io::Result<()> {
     // Split the connection into two halves to process
     // received and sent data concurrently.
     let (reader, mut writer) = conn.into_split();
+    //let mutex_1 = Arc::new(Mutex::new(0));
+    //let mutex_2 = mutex_1.clone();
+
+    let running = Arc::new(AtomicBool::new(true));
+    let running_clone_1 = running.clone();
+    let running_clone_2 = running.clone();
+
+    //let write = writer.write_all(b"First message sent from server to client\n");
+    // The other process is waiting for this, so wait.
+    //try_join!(write)?;
+
     let mut reader = BufReader::new(reader);
 
+   
+    // Loop until EOF
     // Allocate a sizeable buffer for reading.
     // This size should be enough and should be easy to find for the allocator.
     let mut buffer = String::with_capacity(128);
 
-    let write = writer.write_all(b"First message sent from server to client\n");
-    // The other process is waiting for this, so wait.
-    try_join!(write)?;
+    let read_thread_handle = async {
+        let mut total_read = 0;
+        loop {          
+            //let guard = mutex_1.lock();              
+            // Describe the read operation as reading into our big buffer.
+            let read = reader.read_line(&mut buffer);
 
-    // Loop until EOF
-    loop {
-        println!("Reading (in server code) from client");
-        
-        // Describe the write operation as writing our whole message.
-        
-        // Describe the read operation as reading into our big buffer.
-        let read = reader.read_line(&mut buffer);
+            let read_result = read.await;
+            //drop(guard);
 
-        try_join!(read)?; // Done with read
+            if read_result.is_err() {
+                println!("Error reading result in server. Exiting.");
+                break;
+            }
+            total_read += 1;
 
-        if buffer.contains('\0') {
-            break;
-        } else {
-            println!("Got from client: {}", buffer.as_str());
-            buffer.clear();
+            if buffer.contains('\0') {
+                running_clone_2.store(false, Ordering::Relaxed);
+                println!("Reached end on server side. Total read: {}", total_read);
+                break;
+            } else {
+                println!("{} Got from client: {}", get_time_ms(), buffer.as_str());
+                buffer.clear();
+            }
         }
-    }
+        drop(reader);
+    };
 
-    // Dispose of our connection right now and not a moment later because I want to!
-    drop((reader, writer));
+    let write_thread_handle = async {
+        let mut total_written = 0;
+        loop {
+            //let guard = mutex_2.lock();            
+            if !running_clone_1.load(Ordering::Relaxed) {
+                println!("Reached end. Writing acknowledgement and exiting. Total written: {}", total_written);
+                let write = writer.write_all(b"\0\n");
+                let write_result = try_join!(write);
+                if write_result.is_err() {
+                    eprintln!("Error writing final ackknowledgement");
+                }
+                break;
+            }
+            println!("Writing from server to client at {}.", get_time_ms());
+            let write = writer.write_all(b"Server -> client. End not acknowledged yet.\n");
+            // The other process is waiting for this, so wait.
+            let write_result = try_join!(write);
+            if write_result.is_err() {
+                eprintln!("Error writing");
+            }
+            total_written += 1;
+            //drop(guard);
+
+            thread::sleep(time::Duration::from_millis(500));
+        }
+    };
+    
+    join!(write_thread_handle, read_thread_handle);
 
     // Produce our output!
     Ok(())
@@ -66,11 +125,10 @@ pub async fn main(notify: Sender<()>) -> anyhow::Result<()> {
         }
     };
     
-    tokio::spawn(async move {
-        if let Err(e) = handle_conn(conn).await {
-            eprintln!("Error Handling conn: {}", e);
-        }
-    });
+    
+    if let Err(e) = handle_conn(conn).await {
+        eprintln!("Error Handling conn: {}", e);
+    }
 
     anyhow::Result::Ok(())
     //}
